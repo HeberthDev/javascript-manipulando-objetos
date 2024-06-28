77e1dc9a7b193058398080cd93d2d56321c2efd0
HeberthDev <heberthdeveloper@gmail.com>
2024-06-28 08:28:36 -0300

    Clonando objetos

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const listaDeItens = [];
+let listaDeItens = [];
+
+const form = document.getElementById('form-itens');
+const itensInput = document.getElementById('receber-item');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    salvarItem();
+});
+
+function salvarItem() {
+    const comprasItem = itensInput.value;
+
+    listaDeItens.push({
+        valor: comprasItem
+    });
+
+    console.log(listaDeItens);
+}
